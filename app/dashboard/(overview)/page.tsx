59e45b2850bd08f81713f5a62983430db9dcc377
Nelson Gomes <nelson.gomes22@hotmail.com>
2025-11-25 11:03:01 +0000

    Refactor code structure for improved readability and maintainability aswell as SEO and metadata optimization

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,6 +12,12 @@ import {
   CardSkeleton,
 } from "@/app/ui/skeletons";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
 export default async function Page() {
   return (
     <main>
